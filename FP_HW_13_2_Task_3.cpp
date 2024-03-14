008ad43e2b59ca42949d143ff6e7f37b559fbfb8
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-03-14 20:11:37 +0300

    Добавьте файлы проекта.

---
+++ FP_HW_13_2_Task_3.cpp
@@ -0,0 +1,60 @@
+﻿// Используя два указателя на массивы целых
+// чисел, скопировать один массив в другой так, чтобы во
+// втором массиве элементы находились в обратном порядке.
+// Использовать в программе арифметику указателей
+// для продвижения по массиву, а также оператор разыменования.
+
+#include <iostream>
+#include <random>
+#include <windows.h>
+using namespace std;
+
+template<typename T>
+void FillArray(T* arr, int size);
+
+template<typename T>
+void PrintArray(T* arr, int size);
+
+template<typename T>
+void CopyReversedArray(T* oldArr, T* newArr, int size);
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int size = 0;
+	cout << "Указать размер массива для заполнения : ";
+	cin >> size;
+	int* Array = new int[size];
+	int* ReversedArr = new int[size];
+	FillArray(Array, size);
+
+	cout << "Заполненный массив ------------------- >>" << endl;
+	PrintArray(Array, size);
+
+	cout << "Скопировано-ревёрснутый массив ------------------- >>" << endl;
+	CopyReversedArray(Array, ReversedArr, size);
+	PrintArray(ReversedArr, size);
+}
+
+template<typename T>
+void FillArray(T* arr, int size) {
+	random_device random;
+	for (T* i = arr; i < arr + size; ++i) {
+		*i = random() % (100 + 50) - 50;
+	}
+}
+
+template<typename T>
+void PrintArray(T* arr, int size) {	
+		for (T* i = arr; i < arr + size; ++i) 
+			cout << "[ " << *i << " ]";
+	cout << endl;
+}
+
+template<typename T>
+void CopyReversedArray(T* oldArr, T* newArr, int size){
+	for (T* i = oldArr + size - 1, *j = newArr; i >= oldArr; --i, ++j)				
+		*j = *i;		
+}
